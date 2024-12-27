a8b46f30d33c885b5cac636d97245413bdc9405a
Shubham Sharma <samshubham142@gmail.com>
2024-12-27 22:40:45 +0530

    make log-out route as GET route

--- routes/auth.js
+++ routes/auth.js
@@ -4,5 +4,5 @@ const router = express.Router();
 const { signin, login, logout } = require("../controllers/auth");
 router.post("/signin", signin);
 router.post("/login", login);
-router.post("/logout", logout);
+router.get("/logout", logout);
 module.exports = router;
